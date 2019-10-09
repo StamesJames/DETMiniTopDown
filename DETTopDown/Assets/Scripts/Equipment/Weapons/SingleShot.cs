f0747f7adc88fe3d7cc8c7a19c72dd5ff05da3c0
StamesJames <benedict.smit@udo.edu>
2019-10-09 13:40:09 +0200

    WaffenTests
    Added:
    - TestSpieler
      - mit waffen halter
    - TestWaffe (singleshot)
    - TestProjektil (gerade einfache flugbahn)

---
+++ DETTopDown/Assets/Scripts/Equipment/Weapons/SingleShot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleShot : Weapon
+{
+    [SerializeField] Transform shotSpawn;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float fireRate;
+
+    [SerializeField] float shotCd;
+    float nextShot = 0;
+
+    private void Start()
+    {
+        if (fireRate > 0) shotCd = 1 / fireRate;
+        else shotCd = float.PositiveInfinity;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && nextShot <= 0)
+        {
+            Trigger();
+        }
+        else if (nextShot > 0)
+        {
+            nextShot -= Time.deltaTime;
+        }
+    }
+
+    public override void Trigger()
+    {
+        if (shotSpawn && projectile)
+        {
+            Instantiate(projectile, shotSpawn.position, shotSpawn.rotation);
+            nextShot = shotCd;
+        }
+    }
+
+}
