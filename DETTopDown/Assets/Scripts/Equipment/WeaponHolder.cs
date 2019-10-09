05c8c12a3b286fff92caef807e635dd2df76fb27
StamesJames <benedict.smit@udo.edu>
2019-10-09 18:27:10 +0200

    Zielen
    Added:
    - Zielen der Waffe

--- DETTopDown/Assets/Scripts/Equipment/WeaponHolder.cs
+++ DETTopDown/Assets/Scripts/Equipment/WeaponHolder.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class WeaponHolder : MonoBehaviour
 {
+
+    Camera cam;
+
+    Vector2 mousPos;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mousPos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
+
+    private void FixedUpdate()
+    {
+        Vector2 lookVec = mousPos - (Vector2) transform.position;
+        float lookAngle = Mathf.Atan2(lookVec.y, lookVec.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, lookAngle));
+    }
+
 }
