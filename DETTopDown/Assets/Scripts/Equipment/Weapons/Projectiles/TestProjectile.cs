6bc346e2d4662cfd238fb2574147060d3c4b35e9
RedzMarc <marc.nickisch@udo.edu>
2019-10-10 13:29:29 +0200

    Damage und DamagEffect
    Added:
     - IDamageable Interface
     - TestDummy
     - Enum für Schadensarten
     - Particle Effecte bei Damage

--- DETTopDown/Assets/Scripts/Equipment/Weapons/Projectiles/TestProjectile.cs
+++ DETTopDown/Assets/Scripts/Equipment/Weapons/Projectiles/TestProjectile.cs
@@ -16,4 +16,22 @@ public class TestProjectile : MonoBehaviour
         Destroy(this.gameObject, lifeTime);
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IDamageable target = collision.GetComponent<IDamageable>();
+        if (target != null)
+        {
+            target.GetDamaged(20,DAMAGETYPE.NORMAL);
+            GameObject particleObject = target.GetDamageEffect();
+            particleObject.transform.position = this.transform.position;
+
+            particleObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(rb.velocity.y, rb.velocity.x) + 180));
+
+            particleObject.GetComponent<ParticleSystem>().Play();
+        }
+
+        
+        Destroy(this.gameObject);
+    }
+
 }
