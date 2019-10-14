1aae4f5684447fe55ff2b298ccdb1d64c591917b
StamesJames <benedict.smit@udo.edu>
2019-10-14 23:53:26 +0200

    area Protector AI
    Add:
    - Area Protector AI
      - läuft auf den spieler zu falls dieser zu nah an den ursprung des gegner kommt
      - geht wieder zum ursprung wenn der spieler zu weit weg davon geht

---
+++ DETTopDown/Assets/Scripts/Enemy/Dummy/AreaProtector.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AreaProtector : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    [SerializeField] float nextWaypointDistance = 3f;
+    [SerializeField] float protectionRadius = 10f;
+    [SerializeField] float followRadius = 20f;
+    [SerializeField] LayerMask whatToAttack;
+
+    Transform origin;
+    Collider2D targetHit;
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    AIDestinationSetter destSetter;
+    bool onTheWay = false;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (origin == null)
+        {
+            Gizmos.color = new Color(1, 1, 0);
+            Gizmos.DrawWireSphere(transform.position, protectionRadius);
+            Gizmos.color = new Color(1, 0, 1);
+            Gizmos.DrawWireSphere(transform.position, followRadius);
+        }
+        else
+        {
+            Gizmos.color = new Color(1, 1, 0);
+            Gizmos.DrawWireSphere(origin.position, protectionRadius);
+            Gizmos.color = new Color(1, 0, 1);
+            Gizmos.DrawWireSphere(origin.position, followRadius);
+        }
+    }
+
+    private void Awake()
+    {
+        GameObject emptyGameObject = new GameObject();
+        seeker = GetComponent<Seeker>();
+        destSetter = GetComponent<AIDestinationSetter>();
+        origin = Instantiate(emptyGameObject, transform.position, transform.rotation).transform;
+        destSetter.target = origin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!onTheWay)
+        {
+            targetHit = Physics2D.OverlapCircle(origin.position, protectionRadius, whatToAttack);
+            Debug.Log("Scaning for target");
+            if (targetHit != null)
+            {
+                Debug.Log("gefunden: " + targetHit.name);
+                onTheWay = true;
+                destSetter.target = targetHit.transform;
+            }
+        }
+        else if (targetHit)
+        {
+            if ((origin.position - targetHit.transform.position).magnitude > followRadius)
+            {
+                destSetter.target = origin.transform;
+                onTheWay = false;               
+            }
+        }
+
+    }
+}
