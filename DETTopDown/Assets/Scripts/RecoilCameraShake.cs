405d572c02df27f5c6fd649466522a86608e1114
StamesJames <benedict.smit@udo.edu>
2019-10-26 09:41:39 +0200

    Wand Hit Effect
    Wand Hit Effect
    - sieht noch nicht wirklich gut aus
       - es müsste irgendwie bouncen

---
+++ DETTopDown/Assets/Scripts/RecoilCameraShake.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecoilCameraShake : MonoBehaviour
+{
+
+    static RecoilCameraShake _instance;
+
+    public static RecoilCameraShake Instance { get => _instance; }
+
+    private void Awake()
+    {
+
+        if (Instance)
+        {
+            Debug.LogError("mehr als ein recoil shaker");
+        }
+        else
+        {
+            _instance = this;    
+        }
+    }
+    
+    void RecoilShake(Vector2 direction, float amplitude, float time)
+    {
+        
+    }
+
+}
