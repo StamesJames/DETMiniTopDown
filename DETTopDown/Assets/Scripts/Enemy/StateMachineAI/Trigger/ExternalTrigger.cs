0cbb94e889daa0f176ded985f13c0ae12fb7d70f
StamesJames <benedict.smit@udo.edu>
2019-10-29 09:30:44 +0100

    Enemy Room Protection
    Versuche einen Enemy so zu machen, dass er immer wenn der spieler den raum betritt angreift und wenn der spieler damit aus dem ruam raus geht damit aufhört

---
+++ DETTopDown/Assets/Scripts/Enemy/StateMachineAI/Trigger/ExternalTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalTrigger : Trigger
+{
+    public override event GetTriggert OnGettingTriggert;
+
+    public void TriggerMe()
+    {
+        OnGettingTriggert?.Invoke();
+    }
+}
---
+++ DETTopDown/Assets/Scripts/Enemy/StateMachineAI/TriggerEffects/ChasingPlayer.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class ChasingPlayer : TriggerEffect
+{
+    [SerializeField] float speed = 10f;
+    [SerializeField] float stopDistance = 5f;
+    [SerializeField] LayerMask whatIsObstacle;
+
+    delegate void ChasingPlayerAction();
+
+    
+    ChasingPlayerAction stateUpdate;
+    ChasingPlayerAction stateTrigger;
+
+    Transform origin;
+
+
+    [SerializeField] AIController aiController;
+    [SerializeField] AIPath path;
+    [SerializeField] Seeker seeker;
+    [SerializeField] AIDestinationSetter destSetter;
+    bool onTheWay = false;
+
+    private void Awake()
+    {
+        GameObject emptyGameObject = new GameObject();
+        origin = Instantiate(emptyGameObject, transform.position, transform.rotation).transform;
+        destSetter.target = origin;
+        stateUpdate = WaitingUpdate;
+        stateTrigger = WaitingTrigger;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateUpdate?.Invoke();
+    }
+
+    void ChasingTrigger()
+    {
+        stateUpdate = ReturningUpdate;
+        stateTrigger = ReturningTrigger;
+
+        destSetter.target = origin;
+
+        path.endReachedDistance = 0.3f;
+        path.slowdownDistance = 0.3f + 3;
+    }
+
+    void ChasingUpdate()
+    {
+            Vector3 connectionVec = (aiController.CurrentTarget.transform.position - transform.position).normalized;
+            RaycastHit2D lineHit = Physics2D.Linecast(this.transform.position, aiController.CurrentTarget.transform.position + connectionVec, whatIsObstacle);
+            if (lineHit && lineHit.collider.gameObject == aiController.CurrentTarget.gameObject)
+            {
+                path.endReachedDistance = stopDistance;
+                path.slowdownDistance = stopDistance + 3;
+            }
+            else
+            {
+                path.endReachedDistance = 0.3f;
+                path.slowdownDistance = 0.3f + 3;
+
+                path.SearchPath();
+            }
+    }
+
+    void WaitingTrigger()
+    {
+        stateUpdate = ChasingUpdate;
+        stateTrigger = ChasingTrigger;
+        destSetter.target = aiController.CurrentTarget.transform;
+        path.SearchPath();
+        path.endReachedDistance = stopDistance;
+    }
+
+    void WaitingUpdate()
+    {
+
+    }
+
+    void ReturningTrigger()
+    {
+        stateUpdate = ChasingUpdate;
+        stateTrigger = ChasingTrigger;
+        destSetter.target = aiController.CurrentTarget.transform;
+        path.endReachedDistance = stopDistance;
+    }
+
+    void ReturningUpdate()
+    {
+
+    }
+
+    protected override void Trigger()
+    {
+        Debug.Log("Got Triggert");
+        stateTrigger?.Invoke();
+    }
+}
