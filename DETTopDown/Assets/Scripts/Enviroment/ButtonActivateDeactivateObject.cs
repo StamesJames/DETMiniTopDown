9d3892e5e8d37b589bb0c5b2421f23e206a8d234
StamesJames <benedict.smit@udo.edu>
2019-11-05 00:27:24 +0100

    Boss raum und buttons für raum öffnung
    der Bossraum öffnet sich nun erst wenn die buttons gedrückt werden und schließt sich wenn man drin ist

---
+++ DETTopDown/Assets/Scripts/Enviroment/ButtonActivateDeactivateObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivateDeactivateObject : TriggerEffect
+{
+    [SerializeField] GameObject[] objectsToActivate;
+    [SerializeField] bool inverted = false;
+    [SerializeField] ButtonTrigger buttonToListen;
+
+    private new void OnEnable()
+    {
+        base.OnEnable();
+        buttonToListen.ButtonRelese += Relese;
+    }
+
+    private new void OnDisable()
+    {
+        base.OnDisable();
+        buttonToListen.ButtonRelese -= Relese;
+    }
+
+
+    void Relese()
+    {
+        foreach (GameObject item in objectsToActivate)
+        {
+            item.SetActive(inverted);
+        }
+    }
+
+    protected override void Trigger()
+    {
+        foreach (GameObject item in objectsToActivate)
+        {
+            item.SetActive(!inverted);
+        }
+    }
+
+}
---
+++ DETTopDown/Assets/WeaponHolder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHolder : MonoBehaviour
+{
+    [SerializeField] GameObject[] weapons;
+
+    int currentIndex;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("WeaponUp"))
+        {
+            weapons[currentIndex].SetActive(false);
+            currentIndex = (currentIndex + 1) % weapons.Length;
+            weapons[currentIndex].SetActive(true);
+        }
+
+        if (Input.GetButtonDown("WeaponDown"))
+        {
+            weapons[currentIndex].SetActive(false);
+            currentIndex = (currentIndex - 1) % weapons.Length;
+            weapons[currentIndex].SetActive(true);
+        }
+    }
+}
